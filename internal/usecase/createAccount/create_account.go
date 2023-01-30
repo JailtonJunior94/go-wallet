abc839d158a57e14f52f09ca46da7c1f2d8bc4b4
Jailton Junior <jailton.junior94@outlook.com>
2023-01-30 07:36:20 -0300

    avançando no curso de full-cycle módulo: micro serviços

---
+++ internal/usecase/createAccount/create_account.go
@@ -0,0 +1,45 @@
+package createaccount
+
+import (
+	"github.com/jailtonjunior94/go-wallet/internal/entity"
+	"github.com/jailtonjunior94/go-wallet/internal/gateway"
+)
+
+type CreateAccountInputDTO struct {
+	ClientID string
+}
+
+type CreateAccountOutputDTO struct {
+	ID string
+}
+
+type CreateAccountUseCase struct {
+	AccountGateway gateway.AccountGateway
+	ClientGateway  gateway.ClientGateway
+}
+
+func NewCreateAccountUseCase(a gateway.AccountGateway, c gateway.ClientGateway) *CreateAccountUseCase {
+	return &CreateAccountUseCase{
+		AccountGateway: a,
+		ClientGateway:  c,
+	}
+}
+
+func (uc *CreateAccountUseCase) Execute(input *CreateAccountInputDTO) (*CreateAccountOutputDTO, error) {
+	client, err := uc.ClientGateway.Get(input.ClientID)
+	if err != nil {
+		return nil, err
+	}
+
+	account, err := entity.NewAccount(client)
+	if err != nil {
+		return nil, err
+	}
+
+	err = uc.AccountGateway.Save(account)
+	if err != nil {
+		return nil, err
+	}
+
+	return &CreateAccountOutputDTO{ID: account.ID}, nil
+}
